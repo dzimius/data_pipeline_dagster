05ec8c3eab1438f39f1c3b9be4c4a5ff5c3eeb4a
dzimius <pitbull.smietnik@gmail.com>
2025-07-04 13:44:23 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+DRIVER = "SQL Server"
+SERVER = "maciek_d"
+DATABASE = "dagster"
+
+COMMENTS_API_URL = "https://jsonplaceholder.typicode.com/comments"
+POSTS_API_URL = "https://jsonplaceholder.typicode.com/posts"
